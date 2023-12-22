1de814ee948f2c4b200887bd7a19824a5d5d144f
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2023-12-22 19:31:20 +0200

    add tasks

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-// 1
-const categorList = document.getElementById("categories");
-const categorItems = categorList.querySelectorAll('.item');
-console.log(`Кількість категорій: ${categorItems.length}`);
-categorItems.forEach(category => {
-    const h2Title = category.querySelector("h2").textContent;
-    const ulLiList = category.querySelectorAll("ul li");
-    console.log(` заголовок: ${h2Title}, \n кількість: ${ulLiList.length}`)
-})
